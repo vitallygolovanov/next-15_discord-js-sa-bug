c3f7d92bb95b8caf294a7650c14de099bd8aad83
vitallygolovanov <89348452+vitallygolovanov@users.noreply.github.com>
2024-11-30 14:53:35 +0200

    Solution 1: moving to `@discordjs/rest`

--- src/lib/discord/discord.logic.ts
+++ src/lib/discord/discord.logic.ts
@@ -1,10 +1,15 @@
 /**
  * *.logic.ts files are used to define the purely server-side logic of the corresponding *.actions.ts file.
  */
-import { REST, Routes } from "discord.js";
+
+import { REST } from "@discordjs/rest";
+import { Routes } from "discord-api-types/v10";
 import "server-only";
 
-const rest = new REST({ version: '10' });
+const BOT_TOKEN = process.env.DISCORD_BOT_TOKEN ?? '';
+if (!BOT_TOKEN) throw new Error('Discord bot token not found');
+
+const rest = new REST({ version: '10' }).setToken(BOT_TOKEN);
 
 // Call the Nitro Sticker Packs endpoint
 export async function testDiscord() {
