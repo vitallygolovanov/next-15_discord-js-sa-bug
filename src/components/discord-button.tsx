d2155f887fa6f057786bccbfb35e4b499f26ce68
vitallygolovanov <89348452+vitallygolovanov@users.noreply.github.com>
2024-11-30 14:17:28 +0200

    Wrap the server action call in async function to follow the docs and represent my original code more closely.

--- src/components/discord-button.tsx
+++ src/components/discord-button.tsx
@@ -3,7 +3,12 @@ import { testDiscord_action } from "@/lib/discord/discord.actions";
 
 export default function DiscordButton() {
   return (
-    <button className="discord-button" onClick={testDiscord_action}>
+    <button
+      className="discord-button"
+      onClick={async () => {
+        await testDiscord_action();
+      }}
+    >
       <span>Test Discord Action</span>
     </button>
   );
